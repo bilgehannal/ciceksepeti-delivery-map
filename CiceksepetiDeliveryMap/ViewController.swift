767f21dc4025b388ecc48b0c5b1488adfd1f783e
Bilgehan NAL <bnal@ebay.com>
2019-03-18 16:00:36 +0300

    copied from bitbucket

---
+++ CiceksepetiDeliveryMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CiceksepetiDeliveryMap
+//
+//  Created by Yusuf Kamil AK on 13.03.2019.
+//  Copyright © 2019 Yusuf Kamil AK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CiceksepetiDeliveryMapTests/CiceksepetiDeliveryMapTests.swift
@@ -0,0 +1,34 @@
+//
+//  CiceksepetiDeliveryMapTests.swift
+//  CiceksepetiDeliveryMapTests
+//
+//  Created by Yusuf Kamil AK on 13.03.2019.
+//  Copyright © 2019 Yusuf Kamil AK. All rights reserved.
+//
+
+import XCTest
+@testable import CiceksepetiDeliveryMap
+
+class CiceksepetiDeliveryMapTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
